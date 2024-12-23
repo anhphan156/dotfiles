c18bf7ce714a1d13040aa3f0a23de1035c461c68
backspace <anh.phan156@protonmail.com>
2024-12-23 08:28:24 -0500

    update

--- flake.nix
+++ flake.nix
@@ -32,35 +32,10 @@
         inherit src;
 
         installPhase = ''
-          # eww
-          mkdir -p $out/share/eww
-          cp -r $src/config/eww/* $out/share/eww
+          mkdir -p $out/share
+          cp -r --no-preserve=mode,ownership $src/config/* $out/share
           mkdir -p $out/share/eww/variables
           echo '(defvar icon_base_path "${wallpapers.packages.${system}.default}/icons")' > $out/share/eww/variables/iconspath.yuck
-
-          # picom
-          mkdir -p $out/share/picom
-          cp -r $src/config/picom/*.glsl $out/share/picom
-
-          # rofi
-          mkdir -p $out/share/rofi
-          cp -r $src/config/rofi/* $out/share/rofi
-
-          # waybar
-          mkdir -p $out/share/waybar
-          cp -r $src/config/waybar/* $out/share/waybar
-
-          # nvim
-          mkdir -p $out/share/nvim
-          cp -r $src/config/nvim/* $out/share/nvim
-
-          # xmonad
-          mkdir -p $out/share/xmonad
-          cp -r $src/config/xmonad/* $out/share/xmonad
-
-          # awesome
-          mkdir -p $out/share/awesome
-          cp -r $src/config/awesome/* $out/share/awesome
         '';
       };
     };
