bfa1dd2e295d638092eba2464e79b4c1bed23531
backspace <anh.phan156@protonmail.com>
2024-12-24 00:46:45 -0500

    eww bug

--- scripts/eww/leftdockpopulate.nix
+++ scripts/eww/leftdockpopulate.nix
@@ -8,7 +8,7 @@ writeShellApplication {
   name = "leftdockpopulate";
   runtimeInputs = [eww];
   text = ''
-    # shellcheck disable=SC1079,SC2140,SC1078
-    eww update items="${data}"
+    # shellcheck disable=all
+    eww update items='${data}'
   '';
 }
