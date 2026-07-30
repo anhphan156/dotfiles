7eadf708a6a0f3b9e88bb1678d88c3b6cacf2c9c
anhphan156 <anh.phan156@protonmail.com>
2026-07-29 22:50:51 -0400

    lsp keymaps

--- config/neovim/myConfig/lua/keymaps/general/init.lua
+++ config/neovim/myConfig/lua/keymaps/general/init.lua
@@ -32,14 +32,17 @@ vim.keymap.set("v", "<leader>`", "<ESC>`>a`<ESC>`<i`<ESC>", { silent = true, des
 vim.keymap.set("v", "<leader><", "<ESC>`>a><ESC>`<i<<ESC>", { silent = true, desc = "wrap triangle" })
 
 -- Telescope
-vim.keymap.set("n", "<leader>fs", ":Telescope lsp_document_symbols<CR>", { silent = true, desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fr", ":Telescope lsp_references<CR>", { silent = true, desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fd", ":Telescope diagnostics<CR>", { silent = true, desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fs", ":Telescope lsp_dynamic_workspace_symbols<CR>",
+	{ silent = true, desc = "Telescope search symbols" })
+vim.keymap.set("n", "<leader>fr", ":Telescope lsp_references<CR>",
+	{ silent = true, desc = "Telescope search references" })
+vim.keymap.set("n", "<leader>fd", ":Telescope lsp_definitions<CR>",
+	{ silent = true, desc = "Telescope search definitions" })
 
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { silent = true, desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true, desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { silent = true, desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", ":Telescope current_buffer_fuzzy_find<CR>",
-	{ silent = true, desc = "Telescope find files" })
+	{ silent = true, desc = "Telescope buffer fuzzy finder" })
 
 vim.keymap.set("n", "<leader>xd", ":Telescope file_browser<CR>", { silent = true, desc = "Telescope file browser" })
 vim.keymap.set("n", "<leader>pp", ":Telescope project<CR>", { silent = true, desc = "Telescope project browser" })
@@ -49,3 +52,10 @@ vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { silent = true, desc = 
 
 -- Neogit
 vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { silent = true, desc = "Open Neogit" })
+
+
+-- lsp saga
+vim.keymap.set("n", "<leader>sh", ":Lspsaga hover_doc<CR>", { silent = true, desc = "Hover doc" })
+vim.keymap.set("n", "<leader>sa", ":Lspsaga code_action<CR>", { silent = true, desc = "code action" })
+vim.keymap.set("n", "<leader>sd", ":Lspsaga peek_definition<CR>", { silent = true, desc = "peek definition" })
+vim.keymap.set("n", "<leader>sr", ":Lspsaga rename<CR>", { silent = true, desc = "rename" })
